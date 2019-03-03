51a2e386a0ad9c0d3f2aacb3d118c5361559ac20
Glitch (fgoinfobot) <none>
2019-03-03 20:09:23 +0000

    For live bot

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require("./config.json");
-const http = require('http');
+
+
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
@@ -279,5 +292,5 @@ function nextEvent(msg) {
 
 
 
-client.login(config.token);
+client.login(process.env.TOKEN);
 
